95686d57c3d96e24cf6a8ac4f581bf4c8b279c35
Siddharth V Panchanadam <sidpan@eniac.seas.upenn.edu>
2023-04-23 20:52:14 -0400

    Almost Done

--- setupTool.sh
+++ setupTool.sh
@@ -3,6 +3,7 @@
 function errorGuide {
 	printf "To use this tool, the user need to provide the course name, number of assignments (N) and GitHub Repo SSH (optional).\nPlease use the function with the following format:\n"
 	echo -e "bash setupTool \e[3mstartDir\e[0m \e[3mcourseName\e[0m N [\e[3mgithubRepoSSH\e[0m]"
+	printf "Note: Use ~ for the start directory to create the repo in the current directory"
 	exit 1
 }
 
@@ -30,7 +31,7 @@ if [[ -z $(echo "$3" | grep -E "^[0-9]+$") ]]; then
 	errorGuide
 fi
 
-STARTDIR="$1/"
+STARTDIR="$1"
 COURSENAME=$2
 NUMASSIGN=$3
 
@@ -38,17 +39,41 @@ makeFolderName $COURSENAME
 
 printf "Course Name: $COURSENAME\n"
 printf "Number of Assignments: $NUMASSIGN\n\n"
+
 printf "Initializing repo...\n"
 
-#mkdir "$STARTDIR$FOLDERNAME"
-git init -b main "$STARTDIR$FOLDERNAME"
+# Process Path Info
+if [[ $STARTDIR == ~ ]]; then
+	GITPATH=$FOLDERNAME
+elif [[ $STARTDIR == */ ]]; then
+	GITPATH="$STARTDIR$FOLDERNAME"
+else
+	GITPATH="$STARTDIR/$FOLDERNAME"
+fi
+
+git init -b main $GITPATH
 printf "Setting up directories...\n"
 
-mkdir "$STARTDIR$FOLDERNAME/Notes"
-mkdir "$STARTDIR$FOLDERNAME/Assignments"
+mkdir "$GITPATH/Notes"
+printf "# $COURSENAME Notes\nThis is a folder for storing $COURSENAME notes\n" > "$GITPATH/Notes/README.md"
+
+mkdir "$GITPATH/Assignments"
 for file in $(seq -f "%02G" 1 $NUMASSIGN); do
-	mkdir "$STARTDIR$FOLDERNAME/Assignments/Assignment_$file"
-	printf "# Assignment $file\n" > "$STARTDIR$FOLDERNAME/Assignments/Assignment_$file/README.md"
+	mkdir "$GITPATH/Assignments/Assignment_$file"
+	printf "# Assignment $file for $COURSENAME\n" > "$GITPATH/Assignments/Assignment_$file/README.md"
 done
+
+printf "Commiting Changes to Repo\n"
+git -C "$GITPATH" add .
+git -C "$GITPATH" commit -m "Setup Class Repository"
+
+if [[ $# -eq 4 ]]; then
+	SSHCODE=$4
+	printf "Your Specified Repo SSH is: $SSHCODE\n"
+	git -C "$GITPATH" remote add main $SSHCODE
+	git -C "$GITPATH" push main
+fi
+
+printf "DONE\n"
 
 
